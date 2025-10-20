8aeb4a5d497c8c0165f2b0e7e7243e3885dcb449
Sebastian Berrio <sberrio09@gmail.com>
2025-10-19 22:26:09 -0500

    Base project structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {ProductsSectionComponent} from './products/products-section.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, ProductsSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
